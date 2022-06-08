67aa40cecfad608b0053aa92acee3dbc9de38ee6
Rabia <sdetcode@gmail.com>
2022-06-08 14:27:04 +0530

    jaeger integration

--- src/main/java/com/spring/boot/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/com/spring/boot/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,6 +2,7 @@ package com.spring.boot.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -21,6 +22,7 @@ public class MovieCatalogServiceApplication {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
